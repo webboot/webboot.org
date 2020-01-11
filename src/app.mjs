fe4fe685c6b8b6adcb1bee0dc7ca1dc6d37ee02b
Jascha <jascha@jaeh.at>
2020-01-11 05:44:18 +0100

    add packages link to menu

--- src/app.mjs
+++ src/app.mjs
@@ -1,5 +1,5 @@
 export const state = {
   title: '@webboot',
   description: 'solving the tofu problem, one boot at the time.',
-  menu: [{ to: '/', text: 'webboot' }],
+  menu: [{ to: '/#packages', text: 'packages' }, ],
 }

3a5a308b5d6381602a3d87ef4c7c99db6cff2423
Jascha <jascha@jaeh.at>
2020-01-13 08:19:30 +0100

    update menu

--- src/app.mjs
+++ src/app.mjs
@@ -1,5 +1,10 @@
 export const state = {
   title: '@webboot',
   description: 'solving the tofu problem, one boot at the time.',
-  menu: [{ to: '/#packages', text: 'packages' }],
+  menu: [
+    { to: '/#about', text: 'about' },
+    { to: '/#tofu', text: 'tofu' },
+    { to: '/#tosu', text: 'tosu' },
+    { to: '/#support', text: 'support' },
+  ],
 }

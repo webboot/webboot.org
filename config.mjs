d891e4cce9e8137a746f8bd213304d64d5683222
Jascha <jascha@jaeh.at>
2020-01-13 08:19:12 +0100

    more THEME_VARS

--- config.mjs
+++ config.mjs
@@ -10,12 +10,23 @@ export default {
   WEB_ROOT: '/',
   URL: 'webboot.github.io',
 
-  HOIST: ['Gdpr', 'LightSwitch'],
+  HOIST: ['LightSwitch', 'Gdpr'],
 
   THEME_VARS: {
     grey: '#5a5d62',
-    lightgrey: '#c4c7cc',
+    greyLight: '#c4c7cc',
+    greyDark: '#212121',
     red: '#ea4144',
+    redLight: '#ff6e70',
+    redDark: '#c82122',
     white: '#ffffff',
+
+    maxWidth: '1000px',
+
+    widths: {
+      tablet: 500,
+      laptop: 1024,
+      desktop: 1600,
+    },
   },
 }

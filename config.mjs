73586818178cfa6efc8c5ced45b6cf0b6290d7a2
jascha <jascha@jaeh.at>
2020-02-06 20:23:19 +0100

    remove THEME_VARS from config, they are in @webboot/magic-theme dep now

--- config.mjs
+++ config.mjs
@@ -12,24 +12,4 @@ export default {
   CNAME: 'webboot.org',
 
   HOIST: ['LightSwitch', 'Gdpr'],
-
-  THEME_VARS: {
-    grey: '#5a5d62',
-    greyLight: '#c4c7cc',
-    greyDark: '#212121',
-    red: '#ea4144',
-    redLight: '#ff6e70',
-    redDark: '#c82122',
-    white: '#ffffff',
-
-    maxWidth: '1000px',
-
-    widths: {
-      tablet: 500,
-      laptop: 1024,
-      desktop: 1600,
-    },
-
-    fadeDuration: '500ms',
-  },
 }

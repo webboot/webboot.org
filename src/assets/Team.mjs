b4b49b9c727f0fe654a74463f1c606610c250770
jascha <jascha@jaeh.at>
2020-02-06 11:19:57 +0100

    add Team component and team images

---
+++ src/assets/Team.mjs
@@ -0,0 +1,47 @@
+export const View = () =>
+  ul({ class: 'Team' }, [
+    li([
+      Img({ src: '/team/jascha.jpg' }),
+      div([h4(Link({ to: 'https://keybase.io/jascha/' }, 'jascha')), p('development')]),
+    ]),
+    li([
+      Img({ src: '/team/michael.jpg' }),
+      div([h4(Link({ to: 'https://keybase.io/exiledsurfer/' }, 'michael')), p('evangelism')]),
+    ]),
+    li([
+      Img({ src: '/team/mark.jpg' }),
+      div([h4(Link({ to: 'https://keybase.io/easye/' }, 'mark')), p('advise / audit')]),
+    ]),
+  ])
+
+export const style = {
+  width: '100%',
+
+  li: {
+    clear: 'both',
+    display: 'inline-block',
+    margin: '0.5em 0',
+    width: '100%',
+
+    h4: {
+      lineHeight: 1,
+      padding: 0,
+    },
+  },
+
+  img: {
+    float: 'left',
+    margin: '0 1em 0 0',
+    maxWidth: '90px',
+    borderRadius: '100%',
+  },
+
+  '@media screen and (min-width: 800px)': {
+    li: {
+      clear: 'none',
+      width: '33%',
+      float: 'left',
+    },
+
+  },
+}

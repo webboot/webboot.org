a03243e23ce67d54d1728f1633a366b181cdd32f
jascha <jascha@jaeh.at>
2020-02-06 08:55:54 +0100

    move usage to getting-started, fix links in menus

--- src/app.mjs
+++ src/app.mjs
@@ -3,7 +3,7 @@ export const state = {
   description: 'solving the tofu problem, one boot at the time.',
   menu: [
     { to: '/#about', text: 'about' },
-    { to: '/usage/', text: 'usage' },
+    { to: '/getting-started/', text: 'usage' },
     { to: '/tofu/', text: 'tofu' },
     { to: '/tosu/', text: 'tosu' },
   ],

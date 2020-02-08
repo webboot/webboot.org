1ff17e6b503c16fbbfa57337c41408e95275ded9
jascha <jascha@jaeh.at>
2020-02-08 07:03:39 +0100

    src/app: add seo data

--- src/app.mjs
+++ src/app.mjs
@@ -1,11 +1,23 @@
-export const state = {
+export const state = config => ({
   title: '@webboot',
-  description: 'solving the tofu problem, one boot at the time.',
-  image: 'https://webboot.org/webboot-preview.png',
+  description: 'solving the tofu problem, one boot at a time.',
+  seo: {
+    name: '@webboot',
+    url: `https://${config.URL}`,
+    about: 'solving the tofu problem, one boot at a time.',
+    image: `https://${config.URL}${config.WEB_ROOT}webboot-preview.png`,
+    author: {
+      '@type': 'person',
+      name: 'Jascha Ehrenreich',
+      jobTitle: 'Technomancer',
+      image: 'https:/jaeh.at/img/jascha.ehrenreich.jpg',
+      url: 'https://jaeh.at',
+    },
+  },
   menu: [
     { to: '/#about', text: 'about' },
     { to: '/getting-started/', text: 'usage' },
     { to: '/tofu/', text: 'tofu' },
     { to: '/tosu/', text: 'tosu' },
   ],
-}
+})

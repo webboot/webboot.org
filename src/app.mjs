3ff75791b3a298adc19919b80dc040956d960048
jascha <jascha@jaeh.at>
2020-02-08 07:00:23 +0100

    add webboot-preview.jpg and use it for seo

--- src/app.mjs
+++ src/app.mjs
@@ -1,6 +1,7 @@
 export const state = {
   title: '@webboot',
   description: 'solving the tofu problem, one boot at the time.',
+  image: 'https://webboot.org/webboot-preview.png',
   menu: [
     { to: '/#about', text: 'about' },
     { to: '/getting-started/', text: 'usage' },

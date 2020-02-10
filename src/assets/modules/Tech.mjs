6fd59148d98e4563fc21f7309e012334a04a73e2
jascha <jascha@jaeh.at>
2020-02-10 20:59:45 +0100

    update techstack images and add xcf source files

--- src/assets/modules/Tech.mjs
+++ src/assets/modules/Tech.mjs
@@ -17,6 +17,11 @@ export const View = () =>
       div([h4(Link({ to: 'https://cryptohippie.com/' }, 'cryptohippie')), p('vpn')]),
     ]),
     li([
+      Img({ src: '/techstack/keybase.jpg' }),
+      div([h4(Link({ to: 'https://keybase.io/' }, 'keybase')), p('communication')]),
+    ]),
+
+    li([
       Img({ src: '/techstack/aeternity.png' }),
       div([h4(Link({ to: 'https://aeternity.com/' }, 'aeternity')), p('decentralization')]),
     ]),
@@ -51,11 +56,16 @@ export const style = {
     maxWidth: '90px',
   },
 
-  '@media screen and (min-width: 800px)': {
+  '@media screen and (min-width: 500px)': {
     li: {
       clear: 'none',
+      width: '49%',
+    },
+  },
+
+  '@media screen and (min-width: 800px)': {
+    li: {
       width: '33%',
-      float: 'left',
     },
   },
 }

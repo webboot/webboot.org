aef7fe284a712ad90520944c1051c61fa9df74e2
jascha <jascha@jaeh.at>
2020-02-10 23:29:38 +0100

    Support: all images are jpgs now

--- src/assets/modules/Support.mjs
+++ src/assets/modules/Support.mjs
@@ -1,19 +1,19 @@
 export const View = () =>
   List([
     {
-      img: '/support/bwb.png',
+      img: '/support/bwb.jpg',
       to: 'https://bwb.is',
       title: 'bwb',
       text: 'evangelism',
     },
     {
-      img: '/support/metalab.png',
+      img: '/support/metalab.jpg',
       to: 'https://metalab.at',
       title: 'metalab',
       text: 'inspiration',
     },
     {
-      img: '/support/parallele.png',
+      img: '/support/parallele.jpg',
       to: 'https://parallele.at',
       title: 'parallele polis',
       text: 'workspace',

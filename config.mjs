088fd5e5c004d67fc1bf944e19f207506290defb
jascha <jascha@jaeh.at>
2020-02-18 14:43:30 +0100

    config: use @webboot/magic-theme

--- config.mjs
+++ config.mjs
@@ -1,7 +1,7 @@
 export default {
   ROOT: 'src',
   PUBLIC: 'docs',
-  THEME: ['project', 'webboot'],
+  THEME: ['project', '@webboot/magic-theme'],
 
   WEB_ROOT: '/',
   URL: 'webboot.org',
@@ -10,40 +10,4 @@ export default {
   HOIST: ['LightSwitch', 'Gdpr'],
 
   IGNORED_STATIC: ['.xcf'],
-
-  THEME_VARS: {
-    background: {
-      dark: '#212121',
-      light: '#e0e0e0',
-    },
-
-    link: {
-      dark: '#c4c4c4',
-      light: '#212121',
-
-      hover: {
-        dark: '#ea4444',
-        light: '#ea4444',
-      },
-    },
-
-    neutral: '#5a5a5a',
-
-    primary: {
-      dark: '#c82222',
-      light: '#ff7070',
-      neutral: '#ea4444',
-    },
-
-    secondary: {
-      dark: '#22c822',
-      light: '#70ff70',
-      neutral: '#44ea44',
-    },
-
-    text: {
-      dark: '#c4c4c4',
-      light: '#212121',
-    },
-  },
 }
